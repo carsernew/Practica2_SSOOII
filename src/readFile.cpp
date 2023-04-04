c1bb0a4b49332ed583b08145edc96cd9bf16af75
carsernew <Carlos.Fernanz3@alu.uclm.es>
2023-04-04 17:59:19 +0200

    readFile_v1.0.0

---
+++ src/readFile.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include<shared_mutex>
+
+/*******************************************************************************************
+ * 
+ * PROJECT          :       Practica 2 SSOO2                 
+ * 
+ * PROGRAM NAME     :       main.cpp
+ * 
+ * AUTHOR           :       Carlos Fernandez-Aparicio Margoton
+ * 
+ * DATE CREATE      :       04/04/23
+ * 
+ * PURPOSE          :       Lectura y division de las tareas por los hilos 
+ * 
+********************************************************************************************/
