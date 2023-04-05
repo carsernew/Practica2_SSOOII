76be6aba85cd91bbf9d46cf526e8136c988b444d
carsernew <Carlos.Fernanz3@alu.uclm.es>
2023-04-05 13:33:32 +0200

    readFile_v0.2.0

--- src/readFile.cpp
+++ src/readFile.cpp
@@ -26,26 +26,55 @@
  ********************************************************************************************/
 
 
-void readFile(std::string);
+void readFile(std::string,int);
 void threadCreadtion(int);
 
 int main(int argc, char const *argv[])
 {
     std::string file_name(argv[1]);
     //std::string search_word(argv[2]);
-    //int n_threads = atoi(argv[3]);
+    int n_threads = 5;
 
     //threadCreation(n_threads);
-    readFile(file_name);
+    readFile(file_name,n_threads);
     return 0;
 }
 
 void threadCreation(int n_threads)
 {
     std::vector<std::thread> threads(n_threads);
+    for (int i = 0; i < n_threads; i++) {
+        threads[i];  // Crea y asocia cada hilo a la función funcion_hilo
+    }
+
+    for (int i = 0; i < n_threads; i++) {
+        threads[i].join();  // Espera a que cada hilo termine su ejecución
+    }
+
 }
 
-void readFile(std::string file_name)
+void threadCalculating(int n_threads,int lines)
+{
+    int linesthread;
+    int residue;
+    if(n_threads>0){
+        linesthread=lines/n_threads;
+        residue=lines%n_threads;
+
+        std::cout << linesthread <<std::endl;
+        std::cout << residue <<std::endl;
+
+
+
+    }else{
+        std::cerr << " No existen ningun hilo " << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    
+
+}
+
+void readFile(std::string file_name, int n_threads)
 {
 
     std::string root = "books/";
@@ -65,6 +94,8 @@ void readFile(std::string file_name)
         count++;
     }
     std::cout << "El numero de lineas es " << count <<std::endl;
+
+    threadCalculating(n_threads,count);
 
     file.close(); // cerrar el archivo
     exit(EXIT_SUCCESS);
