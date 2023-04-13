8dc359a83917c2e237377d9349b711fda65f156c
carsernew <Carlos.Fernanz3@alu.uclm.es>
2023-04-13 20:10:24 +0200

    Resultados_v.0.1.0

---
+++ src/results.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <shared_mutex>
+#include <fstream>
+
+/*******************************************************************************************
+ *
+ * PROJECT          :       Practica 2 SSOO2
+ *
+ * PROGRAM NAME     :       main.cpp
+ *
+ * AUTHOR           :       Carlos Fernandez-Aparicio Margoton
+ *
+ * DATE CREATE      :       04/04/23
+ *
+ * PURPOSE          :       Se mostraran los datos que se han conseguido en el programa
+ *
+ ********************************************************************************************/
